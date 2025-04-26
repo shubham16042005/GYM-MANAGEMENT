6aa8cd4e45f17a01e24d88e3d54713dd5b7ad02c
shubham16042005 <sg9869259@gmail.com>
2025-04-26 21:53:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const path = require('path');
+const DATA_FILE ='data.json';
 const app = express();
 const port = process.env.PORT || 3000;
 
