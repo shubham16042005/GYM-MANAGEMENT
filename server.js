24e0a2c22ae30e5d530799f247205fcfd33620d9
shubham16042005 <sg9869259@gmail.com>
2025-04-26 21:17:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path =require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -18,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname ,'index.html'));
 });
 
 app.get('/api/members', (req, res) => res.json(members));
